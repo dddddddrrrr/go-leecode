daa92648037de857a051f7773dc9d1d5dff92621
dddddrr <dairui@pyhot.cn>
2024-09-14 03:33:06 +0800

    refine code

--- combinationsum/combinationSum.go
+++ combinationsum/combinationSum.go
@@ -1,8 +1,15 @@
 package combinationsum
 
+import (
+	"sort"
+)
+
 func CombinationSum(candidates []int, target int) [][]int {
 	var result [][]int
 	var current []int
+
+	sort.Ints(candidates)
+
 	var backtrack func(start, remain int)
 	backtrack = func(start, remain int) {
 		if remain == 0 {
@@ -11,15 +18,22 @@ func CombinationSum(candidates []int, target int) [][]int {
 			result = append(result, temp)
 			return
 		}
+
 		for i := start; i < len(candidates); i++ {
 			if candidates[i] > remain {
+				break
+			}
+
+			if i > start && candidates[i] == candidates[i-1] {
 				continue
 			}
+
 			current = append(current, candidates[i])
 			backtrack(i, remain-candidates[i])
 			current = current[:len(current)-1]
 		}
 	}
+
 	backtrack(0, target)
 	return result
 }
